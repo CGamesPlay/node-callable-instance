45e290077883e8c87920d6068a62395f9edaa17a
pabadm <balanpasha9@gmail.com>
2023-09-22 02:49:26 +0200

    exported types

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,8 +1,9 @@
 declare module 'callable-instance' {
     export const CALL: unique symbol;
+    export type SCALL = typeof CALL
 
     type BaseProperty = symbol | string;
-    type CustomProperty = Exclude<BaseProperty, typeof CALL>;
+    type CustomProperty = Exclude<BaseProperty, SCALL>;
 
     type BaseFunc = (...args: any) => any;
     type BaseClass = abstract new (...args: any) => any;
@@ -10,6 +11,7 @@ declare module 'callable-instance' {
 
     type ExtractFuncFromInterface<I extends BaseInterface, P extends BaseProperty> = I[P] extends BaseFunc ? I[P] : never;
 
+    // to avoid error (recursively references itself as a base type)
     interface CloneFuncFromClass<C extends BaseClass, P extends BaseProperty> {
         (...args: Parameters<InstanceType<C>[P]>): ReturnType<InstanceType<C>[P]>;
     }
@@ -20,14 +22,15 @@ declare module 'callable-instance' {
         ? CloneFuncFromClass<C, P>
         : ExtractFuncFromInterface<C, P>;
 
-    interface CallableConstructor {
-        readonly CALL: typeof CALL;
+    export interface CallableConstructor {
+        get CALL(): SCALL;
         new <C extends BaseClass | BaseFunc | BaseInterface, P extends CustomProperty>(property: P): ExtractFunc<C, P>;
-        new <C extends BaseClass | BaseFunc | BaseInterface>(): ExtractFunc<C, typeof CALL>;
+        new <C extends BaseClass | BaseFunc | BaseInterface>(): ExtractFunc<C, SCALL>;
     }
 
     export interface RedefineCall {
-        new <Parent extends BaseClass | BaseFunc | BaseInterface, C extends BaseClass, P extends BaseProperty = typeof CALL>(...args: ConstructorParameters<C>): Omit<InstanceType<C>, P> & ExtractFunc<Parent, P>;
+        new <Parent extends BaseClass | BaseFunc | BaseInterface, C extends BaseClass, P extends BaseProperty = SCALL>(...args: ConstructorParameters<C>):
+            Omit<InstanceType<C>, P> & ExtractFunc<Parent, P>;
     }
 
     const Callable: CallableConstructor;
